e3824cc45d5978d95bc1d9219bd15a3c31497e2e
dawid <dawid@wp.pl>
2025-05-14 10:37:36 +0200

    Added draft for figting gui

--- SwordAndSandals/Form1.Designer.cs
+++ SwordAndSandals/Form1.Designer.cs
@@ -29,18 +29,361 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            label2 = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            progressBar1 = new ProgressBar();
+            progressBar2 = new ProgressBar();
+            progressBar3 = new ProgressBar();
+            progressBar4 = new ProgressBar();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            label7 = new Label();
+            label8 = new Label();
+            button9 = new Button();
+            button10 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(548, 42);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 25);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(92, 35);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 25);
+            label2.TabIndex = 1;
+            label2.Text = "label2";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(488, 90);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(192, 357);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(19, 91);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(198, 345);
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(316, 81);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 4;
+            button1.Text = "Atak";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(391, 118);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 5;
+            button2.Text = "Atak";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(298, 332);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(150, 34);
+            progressBar1.TabIndex = 6;
+            // 
+            // progressBar2
+            // 
+            progressBar2.Location = new Point(298, 411);
+            progressBar2.Name = "progressBar2";
+            progressBar2.Size = new Size(150, 34);
+            progressBar2.TabIndex = 7;
+            // 
+            // progressBar3
+            // 
+            progressBar3.Location = new Point(19, 479);
+            progressBar3.Name = "progressBar3";
+            progressBar3.Size = new Size(150, 34);
+            progressBar3.TabIndex = 8;
+            // 
+            // progressBar4
+            // 
+            progressBar4.Location = new Point(19, 575);
+            progressBar4.Name = "progressBar4";
+            progressBar4.Size = new Size(150, 34);
+            progressBar4.TabIndex = 9;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(316, 133);
+            button3.Name = "button3";
+            button3.Size = new Size(132, 34);
+            button3.TabIndex = 10;
+            button3.Text = "Odpoczynek";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(371, 165);
+            button4.Name = "button4";
+            button4.Size = new Size(132, 34);
+            button4.TabIndex = 11;
+            button4.Text = "Odpoczynek";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(336, 190);
+            button5.Name = "button5";
+            button5.Size = new Size(112, 34);
+            button5.TabIndex = 12;
+            button5.Text = "Naprzód";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(274, 221);
+            button6.Name = "button6";
+            button6.Size = new Size(112, 34);
+            button6.TabIndex = 13;
+            button6.Text = "Naprzód";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(171, 190);
+            button7.Name = "button7";
+            button7.Size = new Size(112, 34);
+            button7.TabIndex = 14;
+            button7.Text = "Cofaj";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(404, 221);
+            button8.Name = "button8";
+            button8.Size = new Size(112, 34);
+            button8.TabIndex = 15;
+            button8.Text = "Cofaj";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(298, 296);
+            label3.Name = "label3";
+            label3.RightToLeft = RightToLeft.No;
+            label3.Size = new Size(109, 25);
+            label3.TabIndex = 16;
+            label3.Text = "Punkty życia";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(42, 451);
+            label4.Name = "label4";
+            label4.Size = new Size(109, 25);
+            label4.TabIndex = 17;
+            label4.Text = "Punkty życia";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(302, 377);
+            label5.Name = "label5";
+            label5.Size = new Size(56, 25);
+            label5.TabIndex = 18;
+            label5.Text = "Mana";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(48, 529);
+            label6.Name = "label6";
+            label6.Size = new Size(56, 25);
+            label6.TabIndex = 19;
+            label6.Text = "Mana";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button10);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(pictureBox1);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button7);
+            groupBox1.Controls.Add(button5);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(progressBar1);
+            groupBox1.Controls.Add(progressBar2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(712, 601);
+            groupBox1.TabIndex = 20;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(button9);
+            groupBox2.Controls.Add(label8);
+            groupBox2.Controls.Add(comboBox2);
+            groupBox2.Controls.Add(pictureBox2);
+            groupBox2.Controls.Add(label2);
+            groupBox2.Controls.Add(label6);
+            groupBox2.Controls.Add(button2);
+            groupBox2.Controls.Add(label4);
+            groupBox2.Controls.Add(progressBar3);
+            groupBox2.Controls.Add(button8);
+            groupBox2.Controls.Add(progressBar4);
+            groupBox2.Controls.Add(button6);
+            groupBox2.Controls.Add(button4);
+            groupBox2.Location = new Point(917, 54);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(945, 661);
+            groupBox2.TabIndex = 21;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "groupBox2";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(42, 510);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(182, 33);
+            comboBox1.TabIndex = 19;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(299, 345);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(182, 33);
+            comboBox2.TabIndex = 20;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(51, 476);
+            label7.Name = "label7";
+            label7.Size = new Size(74, 25);
+            label7.TabIndex = 20;
+            label7.Text = "Zaklecie";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(307, 311);
+            label8.Name = "label8";
+            label8.Size = new Size(74, 25);
+            label8.TabIndex = 21;
+            label8.Text = "Zaklecie";
+            // 
+            // button9
+            // 
+            button9.Location = new Point(518, 349);
+            button9.Name = "button9";
+            button9.Size = new Size(112, 34);
+            button9.TabIndex = 22;
+            button9.Text = "Uzyj";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(234, 521);
+            button10.Name = "button10";
+            button10.Size = new Size(112, 34);
+            button10.TabIndex = 21;
+            button10.Text = "Użyj";
+            button10.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1979, 971);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Margin = new Padding(4, 5, 4, 5);
             Name = "Form1";
-            Text = "Form1";
+            Text = "S";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Button button1;
+        private Button button2;
+        private ProgressBar progressBar1;
+        private ProgressBar progressBar2;
+        private ProgressBar progressBar3;
+        private ProgressBar progressBar4;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private Button button10;
+        private Label label7;
+        private Button button9;
+        private Label label8;
     }
 }
--- SwordAndSandals/Form1.cs
+++ SwordAndSandals/Form1.cs
@@ -6,5 +6,40 @@ namespace SwordAndSandals
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
     }
 }
