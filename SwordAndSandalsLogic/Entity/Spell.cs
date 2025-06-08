f2018519f841d8c4ebdf2c60bd22ea387b470cfb
Dawid <dawid.borkowski37170@gmail.com>
2025-06-08 22:30:01 +0200

    Added spells

---
+++ SwordAndSandalsLogic/Entity/Spell.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwordAndSandalsLogic
+{
+    public class Spell
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public SpellEnum Type { get; set; }
+        public int Mana {  get; set; }
+        public int Damage { get; set; }
+        public int Speed { get; set; } 
+        public int Heal { get; set; }
+        public int Price { get; set; }
+        public Spell() { }
+
+        public Spell(int id, string name, string description, SpellEnum type, int mana, int damage, int speed, int heal, int price)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Type = type;
+            Mana = mana;
+            Damage = damage;
+            Speed = speed;
+            Heal = heal;
+            Price = price;
+        }
+    }
+}
